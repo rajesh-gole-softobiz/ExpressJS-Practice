f58e993b5f79172f79562d12373d4163456cb900
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-23 23:52:34 +0530

    how to send and receive form data

--- 3.Send-Receive-data/index.js
+++ 3.Send-Receive-data/index.js
@@ -11,10 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.post('/user',(req,res)=>{
-    const name = req.body.name;
-    const age = req.body.age;
-    res.send(`Hi ${name}, you are ${age}`)
+app.get('/register',(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post('/register',(req,res)=>{
+    //Receive the value from client to server
+    const fullName = req.body.fullName; // this fullName is as in index.html name=fullName
+    const email = req.body.email;
+    const psw= req.body.psw;
+
+    res.send(`<h1>Your Full Name : ${fullName}, Email : ${email}, Password : ${psw}</h1>`)
+})
+
+app.get('/',(req,res)=>{
+    res.redirect('/register');
 })
 
 // POSTMAN part-> Body ->raw
