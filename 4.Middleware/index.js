2f9ca774246f46d07e4a29f6cfc1dc1329c2989d
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-26 10:11:25 +0530

    updated

--- 4.Middleware/index.js
+++ 4.Middleware/index.js
@@ -5,14 +5,22 @@ const PORT = 3000;
 //Middleware is a function in which there are three things - req obj, res obj, next fun 
 const myMiddleware = (req,res,next) =>{
     console.log('Middleware function');
-    req.curTime =new Date(Date.now());
+    // req.curTime =new Date(Date.now());
     next();
 }
 
+app.use(myMiddleware)
+
 
 app.get('/',myMiddleware,(req,res)=>{
-    console.log('home'+req.curTime);
+    
     res.send('Welcome to Home Page')
+    // console.log('home'+req.curTime);
+})
+
+app.get('/about',myMiddleware,(req,res)=>{
+    
+    res.send('Welcome to About Page')
 })
 
 app.listen(PORT,()=>{
