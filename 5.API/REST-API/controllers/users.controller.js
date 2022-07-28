ff966e2cf7f01db00b0d74b7fc98d2d2e325edfd
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-28 12:18:35 +0530

    POST Method

--- 5.API/REST-API/controllers/users.controller.js
+++ 5.API/REST-API/controllers/users.controller.js
@@ -1,7 +1,19 @@
-const users = require('../models/users.model')
+let users = require('../models/users.model')
+const {v4: uuidv4} = require('uuid');
+
 
 const getAllUsers = (req,res)=>{
     res.status(200).json({users})
 }
 
-module.exports = getAllUsers;
+const createUser = (req,res)=>{
+    const newUser = {
+    id:  uuidv4(),
+    username : req.body.username,
+    email : req.body.email
+    }
+    users.push(newUser)
+    res.status(201).json({users})
+}
+
+module.exports = {getAllUsers,createUser};
--- 5.API/REST-API/routes/users.route.js
+++ 5.API/REST-API/routes/users.route.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const getAllUsers = require('../controllers/users.controller');
+const {getAllUsers, createUser} = require('../controllers/users.controller');
 const router = express.Router();
 
 //GET
 router.get('/', getAllUsers)
+
+
+//POST
+router.post('/', createUser)
+
 
 module.exports = router;
